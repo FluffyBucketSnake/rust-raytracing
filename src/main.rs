de29d7b4a89f97e2c41285f5c9f67771bc02f14f
Bruno Henrique 'FluffyBucketSnake' Glowaski Morais <fluffybucketsnake@outlook.com>
2024-02-26 14:07:50 -0400

    feat: rays

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,58 @@
+mod ray;
 mod vec3;
 
 use std::error::Error;
 use std::io::Write;
 
-use vec3::Vec3;
+use ray::Ray;
+use vec3::{Point3, Vec3};
 
 type Color = Vec3;
 type RenderResult = Result<(), Box<dyn Error>>;
 
 fn main() -> RenderResult {
-    const IMAGE_WIDTH: usize = 256;
-    const IMAGE_HEIGHT: usize = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = ((image_width as f32) / aspect_ratio) as usize;
+
+    let real_aspect_ratio = image_width as f32 / image_height as f32;
+    let viewport_height = 2.0;
+    let viewport_width = viewport_height * real_aspect_ratio;
+
+    let camera_center = Point3::default();
+    let focal_length = 1.0;
+
+    let viewport_u = Vec3::new(viewport_width, 0.0, 0.0);
+    let viewport_v = Vec3::new(0.0, -viewport_height, 0.0);
+    let viewport_du = viewport_u / image_width as f32;
+    let viewport_dv = viewport_v / image_height as f32;
+    let viewport_top_left =
+        camera_center - Vec3::new(0.0, 0.0, focal_length) - (viewport_u + viewport_v) / 2.0;
+    let pixel_top_left = viewport_top_left + 0.5 * (viewport_du + viewport_dv);
+
     let log = &mut std::io::stderr();
     let output = &mut std::io::stdout();
-    write_ppm_header(output, IMAGE_WIDTH, IMAGE_HEIGHT)?;
-    for i in 0..IMAGE_HEIGHT {
-        for j in 0..IMAGE_WIDTH {
-            write_ppm_pixel(
-                output,
-                Color::new(
-                    j as f32 / (IMAGE_WIDTH - 1) as f32,
-                    i as f32 / (IMAGE_HEIGHT - 1) as f32,
-                    0.0,
-                ),
-            )?;
+
+    write_ppm_header(output, image_width, image_height)?;
+    for j in 0..image_height {
+        for i in 0..image_width {
+            let pixel_center = pixel_top_left + (i as f32) * viewport_du + (j as f32) * viewport_dv;
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray::new(pixel_center, ray_direction);
+
+            let color = ray_color(ray);
+            write_ppm_pixel(output, color)?;
         }
-        let _ = writeln!(log, "Scanline progress: {}/{}", i + 1, IMAGE_HEIGHT);
+        let _ = writeln!(log, "Scanline progress: {}/{}", j + 1, image_height);
     }
     output.flush()?;
     Ok(())
+}
+
+fn ray_color(ray: Ray) -> Color {
+    let direction = ray.direction().unit();
+    let a = 0.5 * (direction.y() + 1.0);
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
 
 fn write_ppm_header(output: &mut impl Write, width: usize, height: usize) -> RenderResult {
